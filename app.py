0c78a963f8f7085442034651cc6c278db87a9d39
adstud <166014236+adstud@users.noreply.github.com>
2024-05-18 20:17:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_bigquery_data():
     client = bigquery.Client()
     query = """
         SELECT * FROM `projectcloudmasterid.books.books`
-        LIMIT 10
+    #    LIMIT 10
     """
     query_job = client.query(query)  # Execută interogarea
     results = query_job.result()  # Așteaptă finalizarea interogării
